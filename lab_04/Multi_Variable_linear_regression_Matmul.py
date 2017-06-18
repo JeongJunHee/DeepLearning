63077a9c77b24469d23caeba75783311934073aa
unknown <unizet1@gmail.com>
2017-06-19 03:46:11 +0900

    commit

--- lab_04/Multi_Variable_linear_regression_Matmul.py
+++ lab_04/Multi_Variable_linear_regression_Matmul.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 
-# x, y data
+# x, y data Matrix
 x_data = [[73., 80., 75.],
           [93., 88., 93.],
           [89., 91., 90.],
           [96., 98., 100.],
           [73., 66., 70.]]
-y_data = [[152.], [185.], [180.], [196.], [142.]]
+
+y_data = [[152.],
+          [185.],
+          [180.],
+          [196.],
+          [142.]]
 
 # x, y placeholder
 x = tf.placeholder(tf.float32, shape=[None, 3])
