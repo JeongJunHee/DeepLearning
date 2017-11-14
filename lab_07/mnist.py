db5b10301c8a0a27e2d0ff83d2dd47e1334f4f64
준희-PC\Jun <unizet1@gmail.com>
2017-11-14 13:44:55 +0900

    NN for XOR

--- lab_07/mnist.py
+++ lab_07/mnist.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
 
 nb_classes = 10
 
